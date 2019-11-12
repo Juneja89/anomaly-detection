585b6ed9caed8c812283a56d765d600d63c3ab78
rhdmAdmin <>
2019-11-12 16:30:37 +0530

    {/src/main/java/com/oaknorth/drools/Record.java}

--- src/main/java/com/oaknorth/drools/Record.java
+++ src/main/java/com/oaknorth/drools/Record.java
@@ -9,38 +9,42 @@ public class Record implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
-	@javax.persistence.GeneratedValue(generator = "RECORD_ID_GENERATOR", strategy = javax.persistence.GenerationType.AUTO)
+	@javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "RECORD_ID_GENERATOR")
 	@javax.persistence.Id
 	@javax.persistence.SequenceGenerator(name = "RECORD_ID_GENERATOR", sequenceName = "RECORD_ID_SEQ")
 	private java.lang.Long id;
 
-	@org.kie.api.definition.type.Label(value = "Company Id")
+	@org.kie.api.definition.type.Label("Company Id")
 	private int companyId;
 
-	@org.kie.api.definition.type.Label(value = "Borrower Id")
+	@org.kie.api.definition.type.Label("Borrower Id")
 	private int borrowerId;
 
-	@org.kie.api.definition.type.Label(value = "Loan Id")
+	@org.kie.api.definition.type.Label("Loan Id")
 	private int loanId;
 
-	@org.kie.api.definition.type.Label(value = "Data Version")
+	@org.kie.api.definition.type.Label("Data Version")
 	private java.lang.String dataVersion;
 
-	@org.kie.api.definition.type.Label(value = "Measurement Type")
+	@org.kie.api.definition.type.Label("Measurement Type")
 	private java.lang.String measurementType;
 
-	@org.kie.api.definition.type.Label(value = "Ratio Name")
+	@org.kie.api.definition.type.Label("Ratio Name")
 	private java.lang.String ratioName;
 
-	@org.kie.api.definition.type.Label(value = "Trend")
+	@org.kie.api.definition.type.Label("Trend")
 	private java.lang.String trend;
 
 	@javax.persistence.ElementCollection(fetch = javax.persistence.FetchType.EAGER)
-	@org.kie.api.definition.type.Label(value = "Anomaly")
+	@org.kie.api.definition.type.Label("Anomaly")
 	private java.util.List<java.lang.Integer> anomaly;
 
-	@org.kie.api.definition.type.Label(value = "AbsoluteMajorityAnomaly")
+	@org.kie.api.definition.type.Label("AbsoluteMajorityAnomaly")
 	private int absoluteMajorityAnomaly;
+
+	@javax.persistence.OneToMany(fetch = javax.persistence.FetchType.EAGER, cascade = {javax.persistence.CascadeType.ALL})
+	@org.kie.api.definition.type.Label(value = "Ratio Values")
+	private java.util.List<com.oaknorth.drools.RatioValue> ratioValues;
 
 	public Record() {
 	}
@@ -125,11 +129,21 @@ public class Record implements java.io.Serializable {
 		this.absoluteMajorityAnomaly = absoluteMajorityAnomaly;
 	}
 
+	public java.util.List<com.oaknorth.drools.RatioValue> getRatioValues() {
+		return this.ratioValues;
+	}
+
+	public void setRatioValues(
+			java.util.List<com.oaknorth.drools.RatioValue> ratioValues) {
+		this.ratioValues = ratioValues;
+	}
+
 	public Record(java.lang.Long id, int companyId, int borrowerId, int loanId,
 			java.lang.String dataVersion, java.lang.String measurementType,
 			java.lang.String ratioName, java.lang.String trend,
 			java.util.List<java.lang.Integer> anomaly,
-			int absoluteMajorityAnomaly) {
+			int absoluteMajorityAnomaly,
+			java.util.List<com.oaknorth.drools.RatioValue> ratioValues) {
 		this.id = id;
 		this.companyId = companyId;
 		this.borrowerId = borrowerId;
@@ -140,6 +154,7 @@ public class Record implements java.io.Serializable {
 		this.trend = trend;
 		this.anomaly = anomaly;
 		this.absoluteMajorityAnomaly = absoluteMajorityAnomaly;
+		this.ratioValues = ratioValues;
 	}
 
 }
